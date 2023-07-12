93736a99f254ad03a56e9b7a539188302b26c96a
Alecs Reasonda <momai.reason@gmail.com>
2023-07-12 15:24:50 -0400

    fixed links security

--- src/app/components/CertificateItem.js
+++ src/app/components/CertificateItem.js
@@ -2,7 +2,7 @@ import './styles/certificateitem.scss';
 
 const CertificateItem = ({ certData }) => {
   return (
-    <a className='certificate-item' href={certData.url} target="_blank" rel='noreferrer'>
+    <a className='certificate-item' href={certData.url} target="_blank" rel='noopener noreferrer'>
       <div className='certificate-item__container'>
         <h3>{certData.title}</h3>
         <h4>{certData.detail}</h4 >
