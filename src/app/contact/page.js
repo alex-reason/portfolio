e6d9c4912f3cedde251a5782233fb67dcde88047
Alecs Reasonda <momai.reason@gmail.com>
2023-09-08 12:26:52 -0400

    fixed margins in contact page

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -1,6 +1,6 @@
 import { SocialItem, Navbar, HomeLink, Footer } from '../components';
 import { VscMail } from 'react-icons/vsc';
-import {HiOutlineNewspaper} from 'react-icons/hi';
+import { HiOutlineNewspaper } from 'react-icons/hi';
 import { socialData } from '@/assets/data';
 import './contact.scss';
 
@@ -12,16 +12,16 @@ const Contact = () => {
             <div className='contact'>
                 <h2>Reach out</h2>
                 <div className='contact__container'>
-                  <div className='contact__links'>
-                  <a className='contact__email' href='mailto:alexandrareasonda@gmail.com' target="_blank" rel='noopener noreferrer'>
-                        <VscMail />
-                        <h4>Email me</h4>
-                    </a>
-                    <a className='contact__email' href='tinyurl.com/alex-reason-resume' target="_blank" rel='noopener noreferrer'>
-                        <HiOutlineNewspaper />
-                        <h4>My Resume</h4>
-                    </a>
-                  </div>
+                    <div className='contact__links'>
+                        <a className='contact__email' href='mailto:alexandrareasonda@gmail.com' target="_blank" rel='noopener noreferrer'>
+                            <VscMail />
+                            <h4>Email me</h4>
+                        </a>
+                        <a className='contact__email' href='tinyurl.com/alex-reason-resume' target="_blank" rel='noopener noreferrer'>
+                            <HiOutlineNewspaper />
+                            <h4>My Resume</h4>
+                        </a>
+                    </div>
 
                     <div className='contact__socials'>
                         <h4>Let&apos;s connect!</h4>
